1ec0c98f559ec95faa519cc6722b89b1c4c1d0a8
loviezhang <670238028@qq.com>
2018-03-08 17:06:53 +0800

    commit

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <math.h>
 
 #include "q2dot13.h"
 
@@ -8,6 +10,9 @@ void dump_32(const char* prefix, uint32_t v) {
     printf("%s: ", prefix);
     for (int i = 0; i < 32; i++) {
         printf(" %d", (v >> (31-i)) & 1);
+        if (i % 8 == 0) {
+            printf("  ");
+        }
     }
     printf("\n");
 }
@@ -20,36 +25,30 @@ void dump_16(const char* prefix, uint16_t v) {
     printf("%s: ", prefix);
     for (int i = 0; i < 16; i++) {
         printf(" %d", (v >> (15-i)) & 1);
+        if (i % 8 == 0) {
+            printf("  ");
+        }
     }
     printf("\n");
 }
 
-uint16_t q2dot13_encode(float v) {
-	uint32_t bits = *(uint32_t*)&v;
-	dump_32("bits    ", bits);
-	uint32_t sign = bits >> 31;
-	dump_32("sign    ", sign);
-	uint32_t exponent = (bits >> 23) & 0xff;
-	dump_32("exponent", exponent);
-	uint32_t fraction = bits & 0x007fffff;
-	dump_32("fraction", fraction);
-
-	printf("%.8f\n", (float)fraction/(1<<23));
-
-	//float r = pow(2, 127 - exponent) * 
-
-	return 0;
-}
-
-float q2dot13_decode(uint16_t v) {
-	return 0;
-}
 
 int main(int argc, char** argv) {
-    float a = atof(argv[1]);
-    printf("value = [%.8f]\n", a);
+    printf("max = [%.16f]\n", pos_q2dot13_max);
+    printf("min = [%.16f]\n", pos_q2dot13_min);
+    printf("\n");
 
-	q2dot13_encode(a);
+    float a = atof(argv[1]);
+    printf("value  = [%.8f]\n", a);
+    printf("\n");
+    dump_float("value   ", a);
+    uint16_t en = q2dot13_encode(a);
+    dump_16("encode  ", en);
+
+    float de = q2dot13_decode(en);
+    dump_float("decode  ", a);
+    printf("\n");
+    printf("decode = [%.8f]\n", de);
 
     return 0;
 }
--- q2dot13.h
+++ q2dot13.h
@@ -3,5 +3,53 @@
 
 #include <stdint.h>
 
+const float pos_q2dot13_max = 4 - pow(2, -13);
+const float pos_q2dot13_min = pow(2, -13);
+
+const float neg_q2dot13_max = -1 * pos_q2dot13_max;
+const float neg_q2dot13_min = -1 * pos_q2dot13_min;
+
+
+uint16_t q2dot13_encode(float v) {
+    if (v > pos_q2dot13_max || v < neg_q2dot13_max) {
+        return 0;
+    }
+    if (v < pos_q2dot13_min && v > neg_q2dot13_min) {
+        return 0;
+    }
+
+    uint32_t bits = *(uint32_t*)&v;
+
+    int exponent = ((bits >> 23) & 0xff) - 127;
+    uint32_t fraction;
+    if (exponent >= 0) {
+        fraction = ((bits & 0x007fffff) | 0x00800000) << exponent;
+    } else {
+        fraction = ((bits & 0x007fffff) | 0x00800000) >> (-1*exponent);
+    }
+
+    return ((bits>>16)&0x8000) | ((fraction>>10)&0x6000) | ((fraction>>10)&0x1fff);
+}
+
+float q2dot13_decode(uint16_t v) {
+    if (v == 0) {
+        return 0;
+    }
+
+    int exponent = 2;
+    uint32_t bits = 0;
+
+    bits |= ((uint32_t)v & 0x7fff) << 8;
+
+    do {
+        bits <<= 1;
+        exponent--;
+    } while ((bits & 0x800000) == 0);
+
+    bits &= ~0x800000;
+    bits |= (exponent + 127) << 23 | ((uint32_t)v & 0x8000) << 16;
+
+    return *(float*)&bits;
+}
 
 #endif
